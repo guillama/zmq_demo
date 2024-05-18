495d8516874392cc18c7bafee45c2eeb552a7a5e
Maxime Guillard <guillama@gmail.com>
2024-05-18 19:16:05 +0200

    Improve protobuf generation scripts

--- generate_protobuf.sh
+++ generate_protobuf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python3 -m grpc_tools.protoc -I=protobuf/ --python_out=client/src/gen/ --grpc_python_out="client/src/gen/" protobuf/zmqproto.proto
+set -e
+
+PROTO_FILE="protobuf/zmqproto.proto"
+PROTO_INCLUDE_DIR="protobuf"
+PROTO_DEST_DIR="client/src/gen/"
+
+python3 -m grpc_tools.protoc -I=${PROTO_INCLUDE_DIR} --python_out=${PROTO_DEST_DIR} --grpc_python_out=${PROTO_DEST_DIR} ${PROTO_FILE}
--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,11 @@
 use std::io::Result;
 
+pub const PROTO_FILE: &str = "zmqproto.proto";
+pub const PROTO_INCLUDE_DIR: &str = "../protobuf/";
+
+// This program is compiled and run during the build process.
+// It generate rust code from the protobuf file
 fn main() -> Result<()>{
-    prost_build::compile_protos(&["zmqproto.proto"], &["../protobuf/"])?;
+    prost_build::compile_protos(&[PROTO_FILE], &[PROTO_INCLUDE_DIR])?;
     Ok(())
 }
